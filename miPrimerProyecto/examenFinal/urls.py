8f1c53e0ed95bddc215b8e98f5126c2d5c991608
Hugo <hugovilchezbeltran@gmail.com>
2022-09-26 21:34:53 -0500

    detalle tarea

---
+++ miPrimerProyecto/examenFinal/urls.py
@@ -0,0 +1,10 @@
+from . import views
+from django.urls import path
+
+app_name = 'examenFinal'
+
+urlpatterns = [
+    path('',views.index,name='index'),
+    path('dashboard',views.dashboard,name='dashboard'),
+    path('obtener_info_tarea',views.obtener_info_tarea,name='obtener_info_tarea')
+]
---
+++ miPrimerProyecto/examenFinal/views.py
@@ -0,0 +1,44 @@
+from django.shortcuts import render
+from django.urls import reverse
+from .models import tareasExamen, usuariosFinal
+from django.http import HttpResponse,HttpResponseRedirect,JsonResponse
+import json
+
+# Create your views here.
+def index(request):
+    if request.method == 'POST':
+        nombreUsuario = request.POST.get('nombreUsuario')
+        passwordUsuario = request.POST.get('passwordUsuario')
+        #Validacion de informacion
+        usuario_registrado = 0
+        usuarios_totales = usuariosFinal.objects.all()
+
+        for usuario in usuarios_totales:
+            if usuario.usuario == nombreUsuario and usuario.contra == passwordUsuario:
+                usuario_registrado = 1
+        
+        if usuario_registrado == 1:
+            return HttpResponseRedirect(reverse('examenFinal:dashboard'))
+        else:
+            return render(request,'examenFinal/ingresar.html',{
+                'mensaje':'Los datos ingresados son incorrectos',
+            })
+    return render(request,'examenFinal/ingresar.html')
+
+def dashboard(request):
+    return render(request,'examenFinal/dashboard.html',{
+        'tareas_totales':tareasExamen.objects.all().order_by('id')
+    })
+
+def obtener_info_tarea(request):
+    id_tarea = str(request.GET.get('id'))
+    tarea = tareasExamen.objects.get(id=id_tarea)
+    return JsonResponse({
+        'tarea_detalle': {
+            'id': tarea.id,
+            'fechaCreacion': tarea.fechaCreacion,
+            'fechaEntrega': tarea.fechaEntrega,
+            'descripcion': tarea.descripcion,
+            'estadoTarea': tarea.estadoTarea
+        }
+    })
